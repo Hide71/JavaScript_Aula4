59aa1381d59dce7a017e121442ab20fd93213f38
Hideraldo Rodrigues Marciano <95453443+Hide71@users.noreply.github.com>
2025-12-03 12:33:05 -0300

    Adicionando novo comentário

--- exemplo2/app.js
+++ exemplo2/app.js
@@ -1,7 +1,7 @@
 var main = function (){
 	"use strict";
 	$(".comment-input button").on("click", function(event){
-		console.log("Olá Mundo!");
+		$(".comments").append("<p> Este é um novo comentário!</p>");
 	});
 };
 $(document).ready(main);
