7c199f0e96855134bdeb73f67a7d2ab09025a4e5
Hideraldo Rodrigues Marciano <95453443+Hide71@users.noreply.github.com>
2025-12-04 13:31:24 -0300

    adicionando fade in nos comentários

--- exemplo2/app.js
+++ exemplo2/app.js
@@ -4,7 +4,22 @@ var main = function (){
 		var $novo_comentario;
 		if($(".comment-input input").val() !== ""){
 		    $novo_comentario = $("<p>").text($(".comment-input input").val());
-		    $(".comments").append($novo_comentario);	
+		    $novo_comentario.hide();
+		    $(".comments").append($novo_comentario);
+		    $novo_comentario.fadeIn();
+		    $(".comment-input input").val("");	
+		}
+	});
+	$(".comment-input input").on("keypress", function(event){
+		var $novo_comentario;
+		if(event.keyCode === 13){
+			if ($(".comment-input input").val() !== ""){
+				$novo_comentario = $("<p>").text($(".comment-input input").val());
+				$novo_comentario.hide();
+				$("comments").append($novo_comentario);
+				$novo_comentario.fadeIn();
+				$("comment-input input").val("");
+			}
 		}
 	});
 };
