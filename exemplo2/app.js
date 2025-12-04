15bd502276821759acfb0bf1ef0fea24d41abf99
Hideraldo Rodrigues Marciano <95453443+Hide71@users.noreply.github.com>
2025-12-04 14:07:04 -0300

    refatorando o codigo

--- exemplo2/app.js
+++ exemplo2/app.js
@@ -1,25 +1,22 @@
 var main = function (){
 	"use strict";
-	$(".comment-input button").on("click", function(event){
+	var $adicionar_comentarios = function(){
 		var $novo_comentario;
 		if($(".comment-input input").val() !== ""){
-		    $novo_comentario = $("<p>").text($(".comment-input input").val());
-		    $novo_comentario.hide();
-		    $(".comments").append($novo_comentario);
-		    $novo_comentario.fadeIn();
-		    $(".comment-input input").val("");	
+			$novo_comentario = $("<p>").text($(".comment-input input").val());
+			$novo_comentario.hide();
+			$(".comments").append($novo_comentario);
+			$novo_comentario.fadeIn();
+			$(".comment-input input").val("");
 		}
+	}
+	$(".comment-input button").on("click", function(event){
+		$adicionar_comentarios();
 	});
 	$(".comment-input input").on("keypress", function(event){
 		var $novo_comentario;
 		if(event.keyCode === 13){
-			if ($(".comment-input input").val() !== ""){
-				$novo_comentario = $("<p>").text($(".comment-input input").val());
-				$novo_comentario.hide();
-				$("comments").append($novo_comentario);
-				$novo_comentario.fadeIn();
-				$("comment-input input").val("");
-			}
+			$adicionar_comentarios();
 		}
 	});
 };
