08a8a355641020aba1a5a72f7ad5fabf1a844c7f
Hideraldo Rodrigues Marciano <95453443+Hide71@users.noreply.github.com>
2025-12-04 09:41:04 -0300

    validando o comentário

--- exemplo2/app.js
+++ exemplo2/app.js
@@ -1,8 +1,11 @@
 var main = function (){
 	"use strict";
 	$(".comment-input button").on("click", function(event){
-		var $novo_comentario = $("<p>").text($(".comment-input input").val());
-		$(".comments").append($novo_comentario);
+		var $novo_comentario;
+		if($(".comment-input input").val() !== ""){
+		    $novo_comentario = $("<p>").text($(".comment-input input").val());
+		    $(".comments").append($novo_comentario);	
+		}
 	});
 };
 $(document).ready(main);
